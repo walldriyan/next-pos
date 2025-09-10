4c094962cca460e203715dedac010f3ba26d76fb
walldriyan <walldriyan@gmail.com>
2025-09-11 02:26:20 +0530

    install prisma

---
+++ prisma/migrations/20250910205224_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "companyId" TEXT NOT NULL,
+    "permissions" JSONB NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "User_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_name_key" ON "Company"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
---
+++ src/app/lib/prisma/prisma.ts
@@ -0,0 +1,15 @@
+// src/lib/prisma/prisma.ts
+
+import { PrismaClient } from '@prisma/client'
+
+// PrismaClient එක global scope එකට දානවා
+// මේකෙන් තමයි development එකේදී hot reloading වලදී අලුත් instances හැදෙන්නේ නැති වෙන්නේ
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], // Database queries log කරන්න පුළුවන්
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
