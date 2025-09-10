1470302f3fa9280f1bae6b0e6db861efa6471b3f
walldriyan <walldriyan@gmail.com>
2025-09-11 01:50:23 +0530

    folder strcture created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans bg-white text-black">
+    <div className="font-sans bg-gray-800 text-black">
       hi
     </div>
   );
